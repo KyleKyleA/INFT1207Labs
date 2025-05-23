1c5c16eddc8d2b5a59d0b86cd94e55bcb87837f8
KyleAngeles <kyle.angeles@dcmail.ca>
2025-05-23 12:36:18 -0400

    Create Lab1_Kyle_Mathuran.py

---
+++ SoftwareAutomationKyle/Lab1_Kyle_Mathuran.py
@@ -0,0 +1,14 @@
+#Lab 1 Password Generator
+#Names: Kyle And Mathuran
+#Date: 5/23/25
+#Description: create a program which would generate a random strong
+# password based on the users’ instructions. The user can decide the number of characters in
+# their password along with the number of letters, digits, and special characters in their
+# custom password. The program should be tested to all the extremes to ensure proper
+# functioning.
+
+import random
+import string
+
+
+
