5fb9f3d7ce4a347e50f0bf28cf37d1ca712a56dc
mathuranc1 <70967644+mathuranc1@users.noreply.github.com>
2025-05-23 13:00:51 -0400

    initialize constans and written pseudo-code for functions

--- SoftwareAutomationKyle/Lab1_Kyle_Mathuran.py
+++ SoftwareAutomationKyle/Lab1_Kyle_Mathuran.py
@@ -10,5 +10,35 @@
 import random
 import string
 
+# initialize CONSTANTS
+MIN_LENGTH = 8
+
+MIN_LETTERS = 0
+MAX_LETTERS = 9
+
+MIN_DIGITS = 0
+MAX_DIGITS = 6
+
+MIN_SPECIAL_CHARACTERS = 0
+MAX_SPECIAL_CHARACTERS = 2
+
+UPPER_CASE_LETTERS = ("A", "B", "C",  "D",  "E",  "F",  "G",  "H",  "I",  "J",
+           "K",  "L",  "M",  "N",  "O",  "P",  "Q",  "S",  "T", "U",
+           "V", "W", "X", "Y", "Z")
+
+LOWER_CASE_LETTERS = ("a",  "b",  "c",  "d",  "e",  "f",  "g",  "h", "i", "j",
+                      "k", "l","m", "n", "o",  "p",   "q",   "s",   "t",   "u",
+                      "v",   "w",   "x",   "y",   "z")
+
+# def set_length (str prompt, int min)
+# def set_letters (str prompt, int min, int max)
+# def set_digits (str prompt, int min , int max)
+# def set_special_characters (str prompt, int min, int max)
+# def generate_password(int length, int letters, int digits, int special_characters)
+# def main()
+
+# main()
+# exit()
+
 
 
