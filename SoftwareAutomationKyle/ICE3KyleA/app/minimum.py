78d002bd81727805ba393059035ed3aac41ed326
KyleAngeles <kyle.angeles@dcmail.ca>
2025-06-13 20:02:29 -0400

    fixed the errors and commitng now to finally finished

--- SoftwareAutomationKyle/ICE3KyleA/app/minimum.py
+++ SoftwareAutomationKyle/ICE3KyleA/app/minimum.py
@@ -1,26 +1,22 @@
 # Region function
 class find_minimum(object):
 
-
-    def find_minimum(self):
+    def find_minimum(self, user_input):
         """Takes a space seperated string of numbers, returns the smallest value"""
         valid_numbers = []
-        items = self.split()
 
-        for item in items:
-            try:
-                num = int(item)
-                valid_numbers.append(num)
-            except ValueError:
-                pass
-        if not valid_numbers:
+        if not user_input:
             raise ValueError("No valid numbers entered. ")
 
-        smallest = valid_numbers[0]
-        for num in valid_numbers[1:]:
-            if num < smallest:
-                smallest = num
+        smallest = user_input[0]
+        for num in user_input[1:]:
+            if  type(num) == int :
+                if num < smallest:
+                    smallest = num
+            else:
+                raise ValueError("Smallest number invalid ")
         return smallest
+
 
 if __name__ == '__main__':
     min = find_minimum()
